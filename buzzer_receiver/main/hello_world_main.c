8d68d8898ea943b2376a2b3e95ee416de47ae62b
Justinas Petkauskas <jplabsystems@gmail.com>
2026-05-04 23:23:40 -0400

    started receiver boilerplate

--- buzzer_receiver/main/hello_world_main.c
+++ buzzer_receiver/main/hello_world_main.c
@@ -13,6 +13,18 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
+#define LED_1 3
+#define LED_2 46
+#define LED_3 9
+#define LED_4 10
+#define LED_5 11
+#define LED_6 12
+#define LED_7 13
+#define LED_8 14
+#define BUZZ 8
+#define CLEAR 37
+
+
 void app_main(void)
 {
     printf("Hello world!\n");
