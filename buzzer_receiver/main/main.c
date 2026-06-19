381ad05b27d6b56ed17de68aba7e8cb7c3800988
Justinas Petkauskas <jplabsystems@gmail.com>
2026-06-18 23:45:17 -0400

    millis

--- buzzer_receiver/main/main.c
+++ buzzer_receiver/main/main.c
@@ -63,7 +63,7 @@ void app_main(void)
         time_2 = esp_timer_get_time();
         time_diff = time_2 - time_1;
         printf("Team %d, player %d, time %" PRId64 "\n", team, player, time_2);
-        printf("TIME DIFFERENCE: %" PRId64 ", TEAM WINNER:%d \n\n", time_diff, team);
+        printf("TIME DIFFERENCE: %" PRId64 ", TEAM WINNER:%d \n\n", time_diff / 1000, team);
 
         time_1 = 0;
         time_2 = 0;
